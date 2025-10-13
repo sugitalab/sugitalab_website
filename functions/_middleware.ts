af14da90d99d186500c4f34ff1d0767248923a20
Kentaro Goto <gotoken@syngram.co.jp>
2025-10-13 16:24:28 +0900

    build: add cloudflare pages deply
    - add functions/_middleware.ts to enable basic authentication
    - add "Cache Hugo resources" job to save cpu time

---
+++ functions/_middleware.ts
@@ -0,0 +1,16 @@
+// functions/_middleware.ts
+export const onRequest: PagesFunction = async ({ request, env, next }) => {
+  const header = request.headers.get("Authorization");
+  const need = "Basic " + btoa(`${env.BASIC_AUTH_USER}:${env.BASIC_AUTH_PASS}`);
+  console.log({header, need})
+
+  if (header === need) {
+    return next();
+  }
+
+  return new Response("Unauthorized", {
+    status: 401,
+    headers: { "WWW-Authenticate": 'Basic realm="Protected"' },
+  });
+};
+
