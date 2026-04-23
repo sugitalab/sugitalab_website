1e8e26b3f76db1a588ea18a50683df2c4a46d3cd
Cheng Tan <noinil@gmail.comm>
2026-04-23 11:40:29 +0900

    clean up unnecessary files.

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -2,7 +2,6 @@
 export const onRequest: PagesFunction = async ({ request, env, next }) => {
   const header = request.headers.get("Authorization");
   const need = "Basic " + btoa(`${env.BASIC_AUTH_USER}:${env.BASIC_AUTH_PASS}`);
-  console.log({header, need})
 
   if (header === need) {
     return next();
--- static/projects/bie/wp-content/themes/bie-projects/js/accordion-list.js
+++
@@ -1,8 +0,0 @@
-(function ($) {
-  const toggleModalClass = (event) => {
-    $(event.target).toggleClass('is-active');
-    $(event.target).children('ul').slideToggle(300);
-  }
-
-  $('.bie-accordion-list > li').on('click', toggleModalClass);
-}(jQuery));
